f55154a4cc932523907e0f4a83bf43adba273543
ilxomkh <94556237+ilxomkh@users.noreply.github.com>
2025-06-09 12:14:20 +0500

    sec

---
+++ language-test-widget/dist/runtime/translations/uzcryl.ts
@@ -0,0 +1,11 @@
+System.register([], function (_export) {
+  return {
+    execute: function () {
+      _export({
+        _widgetLabel: 'Синов Виджет',
+        description: 'Тил ўзгартирилганда бу матн ўзгаради.',
+        chooseLanguage: 'Тилни танланг'
+      });
+    }
+  };
+});
